feffed92f8c537b3731e318bfeaa3cb2ea59f283
SamElomari <90078380+SamElomari@users.noreply.github.com>
2025-04-04 22:24:04 -0400

    Update and rename test_ assignment_3.py to test_assignment_3.py

--- test/test_assignment_3.py
+++ test/test_assignment_3.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'main')))
+
 import numpy as np
 from assignment_3 import (
     gaussian_elimination_with_back_substitution,
